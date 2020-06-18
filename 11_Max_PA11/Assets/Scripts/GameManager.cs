95132938a2179be059daee017e817e25b0d8b398
Maxi14th <64114695+Maxi14th@users.noreply.github.com>
2020-06-18 17:38:43 +0800

    pa11 done pui chao ni lao ma mh
    yes

---
+++ 11_Max_PA11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager thisManager;
+    public Text ScoreText;
+    public float ScoreValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+        ScoreValue++;
+        ScoreText.text = "Score : " + ScoreValue;
+    }
+}
---
+++ 11_Max_PA11/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private float yMin = -3.9f, yMax = 3.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+        float yLock = Mathf.Clamp(transform.position.y, -3.9f, 3.9f);
+
+        transform.position = new Vector3(transform.position.x, yLock, transform.position.z);
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
