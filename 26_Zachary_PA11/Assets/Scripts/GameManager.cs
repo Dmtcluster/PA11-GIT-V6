4bc48dcf27bf956ca46ffe183f88b1e931b7f1bf
NezuNyan <64115041+NezuNyan@users.noreply.github.com>
2020-06-26 13:45:13 +0800

    updated

---
+++ 26_Zachary_PA11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private float score;
+
+    GameManager thismanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thismanager = this;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void increasescore()
+    {
+        score += 10;
+    }
+}
---
+++ 26_Zachary_PA11/Assets/Scripts/UI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Text txtscore;
+    public GameObject player;
+
+
+    private float score;
+
+    public GameObject obstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obstacle.transform.position.x <= player.transform.position.x)
+        {
+            score += 10;
+        }
+
+        
+
+        txtscore.text = "Score: " +score;
+    }
+
+    void increasescore()
+    {
+        
+    }
+}
