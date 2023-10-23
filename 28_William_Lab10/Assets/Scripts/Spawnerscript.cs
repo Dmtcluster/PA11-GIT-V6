ce5486428ee999de53307173cc61ac71c66c2c81
Inzanity <92455829+Inzanity7001@users.noreply.github.com>
2023-10-23 11:11:24 +0800

    Updated Prototype
    Done By William

---
+++ 28_William_Lab10/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject_Red;
+    public GameObject SpawnObject_Brown;
+    public GameObject SpawnObject_Green;
+
+    public Text scoreText;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + ObstacleMovement.score;
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        int RandomIndex = Random.Range(0, 3);
+        GameObject spawnObject;
+
+        switch (RandomIndex)
+        {
+            case 0:
+                spawnObject = SpawnObject_Red;
+                break;
+            case 1:
+                spawnObject = SpawnObject_Brown;
+                break;
+            case 2:
+                spawnObject = SpawnObject_Green;
+                break;
+            default:
+                spawnObject = SpawnObject_Green;
+                break;
+        }
+
+        Instantiate(spawnObject, transform.position, transform.rotation);
+    }
+}
